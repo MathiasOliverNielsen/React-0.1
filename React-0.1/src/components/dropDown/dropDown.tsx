b1add39035de880d85e4cbbcd6a2b4df196066f1
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-06 10:22:53 +0100

    refactor: update DropDown component to use SCSS modules and named export

--- React-0.1/src/components/dropDown/dropDown.tsx
+++ React-0.1/src/components/dropDown/dropDown.tsx
@@ -1,1 +1,15 @@
-export DropDown from "./dropDown";
+import dropDownStyles from "./dropDown.module.scss";
+
+function DropDown() {
+  return (
+    <div className={dropDownStyles.dropDown}>
+      <button className={dropDownStyles.dropDownButton}>Menu</button>
+      <div className={dropDownStyles.dropDownContent}>
+        <a href="#profile">Profile</a>
+        <a href="#settings">Settings</a>
+        <a href="#logout">Logout</a>
+      </div>
+    </div>
+  );
+}
+export { DropDown };
